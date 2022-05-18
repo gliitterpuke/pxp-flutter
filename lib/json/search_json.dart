940bacf1598df47960c02f57745909d31bf5645f
Katherine Wang <katherinewwang@gmail.com>
2022-05-18 14:51:38 -0700

    add genre and book search

--- lib/json/search_json.dart
+++ lib/json/search_json.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_icons/flutter_icons.dart';
+
 const List searchJson = [
   {
     "img": "assets/images/narnia.jpg",
@@ -15,4 +17,118 @@ const List searchJson = [
     "img": "assets/images/sky.jpg",
     "title": "Morning Sky",
   }
+];
+
+const List<Map<String, dynamic>> tags = [
+  {
+    "id": 1,
+    "icon": MaterialCommunityIcons.sword,
+    "tag": "PxP Picks",
+  },
+  {
+    "id": 2,
+    "icon": MaterialCommunityIcons.target,
+    "tag": "Action",
+  },
+  {
+    "id": 3,
+    "icon": MaterialCommunityIcons.sword,
+    "tag": "Adventure",
+  },
+  {
+    "id": 4,
+    "icon": MaterialCommunityIcons.drama_masks,
+    "tag": "Comedy",
+  },
+  {
+    "id": 5,
+    "icon": MaterialCommunityIcons.drama_masks,
+    "tag": "Contemporary Lit",
+  },
+  {
+    "id": 6,
+    "icon": FontAwesome.magic,
+    "tag": "Fan-fiction",
+  },
+  {
+    "id": 7,
+    "icon": FontAwesome.magic,
+    "tag": "Fantasy",
+  },
+  {
+    "id": 8,
+    "icon": MaterialCommunityIcons.target,
+    "tag": "Historical Fiction",
+  },
+  {
+    "id": 9,
+    "icon": MaterialCommunityIcons.target,
+    "tag": "Horror",
+  },
+  {
+    "id": 10,
+    "icon": FontAwesome.search,
+    "tag": "Mystery",
+  },
+  {
+    "id": 11,
+    "icon": FontAwesome.search,
+    "tag": "New Adult",
+  },
+  {
+    "id": 12,
+    "icon": Entypo.open_book,
+    "tag": "Non-fiction",
+  },
+  {
+    "id": 13,
+    "icon": FontAwesome.search,
+    "tag": "Paranormal",
+  },
+  {
+    "id": 14,
+    "icon": MaterialCommunityIcons.heart_multiple_outline,
+    "tag": "Romance",
+  },
+  {
+    "id": 15,
+    "icon": MaterialCommunityIcons.space_invaders,
+    "tag": "Sci-fi",
+  },
+  {
+    "id": 16,
+    "icon": Entypo.open_book,
+    "tag": "Thriller",
+  },
+];
+
+const List<Map<String, dynamic>> books = [
+  {
+    "id": 1,
+    "img": "assets/images/nonfiction.png",
+    "title": "Think Outside the Box",
+    "genre": "Non-fiction",
+    "author": "Author"
+  },
+  {
+    "id": 2,
+    "img": "assets/images/narnia.jpg",
+    "title": "The Lion, the Wtich, and the Wardrobe",
+    "genre": "Fantasy",
+    "author": "C.S. Lewis"
+  },
+  {
+    "id": 3,
+    "img": "assets/images/none.jpg",
+    "title": "And Then There Were None",
+    "genre": "Mystery",
+    "author": "Agatha Christie"
+  },
+  {
+    "id": 4,
+    "img": "assets/images/primarythreat.jpg",
+    "title": "Primary Threat",
+    "genre": "Action",
+    "author": "Jack Mars"
+  },
 ];
--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pxp_flutter/json/search_json.dart';
 import 'package:pxp_flutter/pages/book_detail.dart';
+import 'package:pxp_flutter/constants/Theme.dart';
 
 class SearchPage extends StatefulWidget {
   @override
@@ -8,13 +9,44 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  List<Map<String, dynamic>> _bookList = [];
+  bool _emptyBooks = false;
+  @override
+  initState() {
+    _bookList = [];
+    _emptyBooks = false;
+    super.initState();
+  }
+
+  void _runFilter(String enteredKeyword) {
+    List<Map<String, dynamic>> results = [];
+    bool emptyResults = false;
+    if (enteredKeyword.isEmpty) {
+      results = [];
+    } else {
+      results = books
+          .where((book) => book["title"]
+              .toLowerCase()
+              .contains(enteredKeyword.toLowerCase()))
+          .toList();
+    }
+
+    // Refresh the UI
+    setState(() {
+      if (enteredKeyword.isNotEmpty && results.length == 0) {
+        _emptyBooks = true;
+        _bookList = results;
+      } else {
+        _bookList = results;
+        _emptyBooks = false;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: getAppBar(),
-      body: getBody(),
-    );
+        backgroundColor: Colors.black, appBar: getAppBar(), body: getBody());
   }
 
   PreferredSizeWidget getAppBar() {
@@ -29,6 +61,7 @@ class _SearchPageState extends State<SearchPage> {
             borderRadius: BorderRadius.circular(5),
             color: Colors.grey.withOpacity(0.25)),
         child: TextField(
+          onChanged: (value) => _runFilter(value),
           decoration: InputDecoration(
               border: InputBorder.none,
               hintText: "Search",
@@ -42,74 +75,109 @@ class _SearchPageState extends State<SearchPage> {
   }
 
   Widget getBody() {
-    var size = MediaQuery.of(context).size;
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.only(top: 10, left: 18, right: 18),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Top Searches",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 12,
-            ),
-            Column(
-                children: List.generate(searchJson.length, (index) {
-              return GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                      context, MaterialPageRoute(builder: (_) => BookDetail()));
-                },
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 10),
-                  child: Row(
-                    children: [
-                      Container(
-                        child: Row(
-                          children: [
-                            Stack(
-                              children: [
-                                Container(
-                                  height: 80,
-                                  width: 120,
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(5),
-                                      image: DecorationImage(
-                                          image: AssetImage(
-                                              searchJson[index]['img']),
-                                          fit: BoxFit.cover)),
-                                ),
-                                Container(
-                                    height: 70,
-                                    width: 120,
-                                    decoration: BoxDecoration(
-                                        color: Colors.black.withOpacity(0.2)))
-                              ],
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Container(
-                              width: (size.width - 30) * 0.6,
-                              child: Text(
-                                searchJson[index]['title'],
-                                style: TextStyle(
-                                    fontSize: 15, fontWeight: FontWeight.w600),
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        children: [
+          Expanded(
+              child: _bookList.isNotEmpty
+                  ? ListView.builder(
+                      itemCount: _bookList.length,
+                      itemBuilder: (context, index) => Card(
+                        key: ValueKey(_bookList[index]["id"]),
+                        color: Colors.black,
+                        elevation: 4,
+                        child: ListTile(
+                            leading: Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 10, top: 8.0),
+                              child: Container(
+                                height: 60,
+                                width: 60,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5),
+                                    image: DecorationImage(
+                                        image:
+                                            AssetImage(_bookList[index]['img']),
+                                        fit: BoxFit.cover)),
                               ),
-                            )
-                          ],
-                        ),
+                            ),
+                            title: Text(
+                              _bookList[index]['title'],
+                              style: TextStyle(fontWeight: FontWeight.w600),
+                            ),
+                            subtitle: Text(_bookList[index]['author'])),
                       ),
-                    ],
-                  ),
-                ),
-              );
-            }))
-          ],
-        ),
+                    )
+                  : Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                          _emptyBooks == true
+                              ? const Padding(
+                                  padding: EdgeInsets.only(
+                                      top: 10, left: 30, bottom: 20),
+                                  child: Text(
+                                    "No results found -- explore our genres!",
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                )
+                              : const Padding(
+                                  padding: EdgeInsets.only(
+                                      top: 10, left: 30, bottom: 20),
+                                  child: Text(
+                                    "Explore our genres",
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                ),
+                          Expanded(
+                            child: GridView.count(
+                              childAspectRatio: 0.8,
+                              crossAxisCount: 4,
+                              children: List.generate(tags.length, (index) {
+                                return Column(children: [
+                                  CircleAvatar(
+                                    radius: 30,
+                                    backgroundColor: const Color(0xff161618),
+                                    child: IconButton(
+                                      icon: Icon(
+                                        tags[index]['icon'],
+                                        color: pxpColors.secondaryT,
+                                      ),
+                                      onPressed: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (_) =>
+                                                    const BookDetail()));
+                                      },
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: SizedBox(
+                                      width: 80,
+                                      child: Center(
+                                        child: Text(
+                                          tags[index]['tag'],
+                                          overflow: TextOverflow.ellipsis,
+                                          maxLines: 2,
+                                          style: const TextStyle(
+                                              color: pxpColors.white,
+                                              fontSize: 14,
+                                              fontWeight: FontWeight.w600),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ]);
+                              }),
+                            ),
+                          )
+                        ])),
+        ],
       ),
     );
   }
